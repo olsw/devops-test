eeea727b49857e549f531f732674e451af93a185
olsw <ollywhiteman@gmail.com>
2020-10-04 18:52:21 +0100

    Added in a range of IPs for GitHub ssh access, allowed external access to all on port 80

--- environment.tf
+++ environment.tf
@@ -49,10 +49,25 @@ resource "aws_security_group" "devops-access" {
     }
 
     ingress {
-        from_port   = 3000
-        to_port     = 3000
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = var.GitHubIP
+        ipv6_cidr_blocks = var.GitHubIPv6
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 3000
+        to_port   = 3000
+        protocol  = "tcp"
+        self      = true
     }
 
 }
