846ab2b65ec5bffea9ae5f64366f2cd0e73d872c
olsw <ollywhiteman@gmail.com>
2020-10-04 18:56:04 +0100

    Opening up port 22 completely as the range from microsoft doesnt seem to be working

--- environment.tf
+++ environment.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "devops-access" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = [var.MY_IP]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
